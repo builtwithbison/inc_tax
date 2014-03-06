a9ea29a91e54148487b39f7ee93e08eb4adddacf
Jason Varga <varga.jason@gmail.com>
2014-03-06 18:37:52 -0500

    i18n support

--- _add-ons/inc_tax/mod.inc_tax.php
+++ _add-ons/inc_tax/mod.inc_tax.php
@@ -6,6 +6,9 @@ class Modifier_inc_tax extends Modifier
 	{
 		$config = $this->addon->api('bison')->getBisonConfig();
 		$tax = $config['tax_rate'];
-		return number_format($value + $value * $tax/100, 2);
+		$decimal = $config['decimal_point'];
+		$separator = $config['thousands_separator'];
+		$value = str_replace($decimal, '.', str_replace($separator, '', $value));
+		return number_format($value + $value * $tax/100, 2, $decimal, $separator);
 	}
 }
